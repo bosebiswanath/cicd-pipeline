4d20293a8d8fe8450d3395c74f9cb626e157ccdd
bosebiswanath <bose.biswanathcse@gmail.com>
2023-08-03 12:17:07 +0530

    dummy commit

--- server.js
+++ server.js
@@ -2,17 +2,13 @@ const express = require('express')
 const app = express()
 
 
-
-
-
-
-
 app.get("/", (req, res) =>{
     res.json({
         status: 200,
         message: "Hello World"
     })
 })
+
 
 app.get("/product", (req, res) =>{
     res.json({
